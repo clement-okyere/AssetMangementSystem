fc702cec9a6c0e63ca159f746734a3f2fcaeb444
okyere <clement.okyere@gmail.com>
2021-04-19 21:14:25 +0000

    added asset class

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Domain/Models/Asset.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.February2021.Domain.Models
+{
+    public class Asset
+    {
+        public int ID { get; set; }
+        public string AssetName { get; set; }
+        public Department Department { get; set; }
+        public string CountryofDepartment { get; set; }
+        public  string EmailAddressofDepartment { get; set; }
+        private DateTime purchaseDate { get; set; }
+        public DateTime PurchaseDate {
+            get { return purchaseDate; }
+            set
+            {
+
+                value = purchaseDate.ToUniversalTime();
+            }
+
+        }
+        [DefaultValue(false)]
+        public bool broken { get; set; }
+    }
+
+   public enum Department
+    {
+        HQ,
+        Store1,
+        Store2,
+        Store3,
+        MaintenanceStation
+    }
+}
