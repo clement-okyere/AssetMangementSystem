eb703430e27b17e3c54502343c4aebd375d04f89
okyere <clement.okyere@gmail.com>
2021-04-20 12:44:11 +0000

    added http client for countries

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Data/CountryClient/CountryClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.February2021.Data.CountryClient
+{
+    public class CountryClient 
+    {
+        private HttpClient _client;
+
+        public CountryClient(HttpClient client)
+        {
+            _client = client;     
+        }
+
+        public async Task<bool> GetCountryByName(string country)
+        {
+
+            var response = await _client.GetAsync($"/rest/v2/name/{country}?fullText=true");
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                return false;
+
+            return true;
+        }
+    }
+}
