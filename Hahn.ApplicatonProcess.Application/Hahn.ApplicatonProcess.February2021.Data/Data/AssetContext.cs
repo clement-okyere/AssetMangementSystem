249bef7d6828660c46d2dbf5f4c96abaa7d6951a
okyere <clement.okyere@gmail.com>
2021-04-20 14:08:24 +0000

    added database context for asset class

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Data/Data/AssetContext.cs
@@ -0,0 +1,21 @@
+﻿using Hahn.ApplicatonProcess.February2021.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.February2021.Data.Data
+{
+    public class AssetContext : DbContext
+    {
+        public AssetContext(DbContextOptions<AssetContext> options)
+            : base(options)
+        {
+
+        }
+
+       public DbSet<Asset> Assets {get; set; }
+    }
+}
