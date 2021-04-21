d0d16bf8dcb510505e4f58fa7d00df11cb8e82ac
okyere <clement.okyere@gmail.com>
2021-04-21 12:45:14 +0000

    added validation filter

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Data/ValidationFilters/ValidateModelAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.February2021.Data.ValidationFilters
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
