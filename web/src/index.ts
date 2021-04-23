7fb99c888fa848c05a2f706f3b4ad3be235a9926
okyere <clement.okyere@gmail.com>
2021-04-23 17:38:01 +0000

    removed aurelia app

--- web/src/index.ts
+++
@@ -1,8 +0,0 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
-import {PLATFORM} from 'aurelia-pal';
-
-export function configure(config: FrameworkConfiguration): void {
-  config.globalResources([
-    PLATFORM.moduleName('./elements/hello-world')
-  ]);
-}
