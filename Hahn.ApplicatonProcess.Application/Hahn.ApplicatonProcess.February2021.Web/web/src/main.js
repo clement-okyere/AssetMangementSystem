7735d18230e709842937825fc0f7ca5d790b8326
okyere <clement.okyere@gmail.com>
2021-04-28 09:27:02 +0000

    added aurelia validation plugin

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/main.js
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/main.js
@@ -2,8 +2,9 @@ import * as environment from '../config/environment.json';
 import { PLATFORM } from 'aurelia-pal';
 export function configure(aurelia) {
     aurelia.use
-        .standardConfiguration()
-        .feature(PLATFORM.moduleName('resources/index'));
+      .standardConfiguration()
+      .plugin(PLATFORM.moduleName("aurelia-validation"))
+      .feature(PLATFORM.moduleName("resources/index"));
     aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
     if (environment.testing) {
         aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
