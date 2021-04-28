2a62401b94f40c7fd145f5dc164bd0ca68b21bc1
okyere <clement.okyere@gmail.com>
2021-04-28 10:48:32 +0000

    configured aurelia-I18N in main

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/main.ts
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/main.ts
@@ -1,6 +1,8 @@
 import {Aurelia} from 'aurelia-framework';
 import * as environment from '../config/environment.json';
 import { PLATFORM } from 'aurelia-pal';
+import { TCustomAttribute } from "aurelia-i18n";
+import XHR from "i18next-xhr-backend";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
@@ -9,6 +11,29 @@ export function configure(aurelia: Aurelia): void {
     .standardConfiguration()
     .plugin(PLATFORM.moduleName("aurelia-validation"))
     .plugin(PLATFORM.moduleName("aurelia-dialog"))
+    .plugin(PLATFORM.moduleName("aurelia-i18n"), (instance) => {
+      let aliases = ["t", "i18n"];
+      // add aliases for 't' attribute
+      TCustomAttribute.configureAliases(aliases);
+
+      // register backend plugin
+      // instance.i18next.use(Backend.with(aurelia.loader));
+      // register backend plugin
+      instance.i18next.use(XHR);
+
+      // adapt options to your needs (see http://i18next.com/docs/options/)
+      // make sure to return the promise of the setup method, in order to guarantee proper loading
+      return instance.setup({
+        backend: {
+          // <-- configure backend settings
+          loadPath: "/locales/{{lng}}/{{ns}}.json", // <-- XHR settings for where to get the files from
+        },
+        attributes: aliases,
+        lng: "de",
+        fallbackLng: "en",
+        debug: false,
+      });
+    })
     .feature(PLATFORM.moduleName("resources/index"));
 
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
