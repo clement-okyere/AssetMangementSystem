984aaf45cc70756cddab53bb4b78baf50a80c8f0
okyere <clement.okyere@gmail.com>
2021-04-28 08:39:37 +0000

    injected validation controller

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/pages/registration-form.ts
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/pages/registration-form.ts
@@ -1,4 +1,20 @@
+import { inject } from "aurelia-dependency-injection";
+import {
+  Validator,
+  ValidationController,
+  validateTrigger,
+  ValidationControllerFactory,
+  ValidationRules,
+} from "aurelia-validation";
 
+@inject(ValidationControllerFactory, Validator, ValidationControllerFactory)
 export class RegistrationForm {
-    
+  controller: ValidationController;
+
+  constructor(
+    controllerFactory,
+  ) {
+    this.controller = controllerFactory.createForCurrentScope();
+    this.controller.validateTrigger = validateTrigger.changeOrBlur;
+  }
 }
