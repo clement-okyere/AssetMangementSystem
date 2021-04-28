6deffae15601ce7e9f5038cc7b694662b4da818b
okyere <clement.okyere@gmail.com>
2021-04-28 09:27:52 +0000

    added country validation function

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/utils/helper.ts
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/web/src/utils/helper.ts
@@ -1,8 +1,20 @@
 import * as moment from "moment";
-
+import { HttpClient } from "aurelia-http-client";
 
 export const getYearDifference = (purchaseDate: Date): number => {
   let dateBought = moment(purchaseDate, "YYYY-MM-DD");
   return moment().diff(dateBought, "years");
 };
 
+export const validateCountry = async (country: string): Promise<boolean> => {
+  try {
+    const API_ENDPOINT = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+    const httpClient: HttpClient = new HttpClient();
+
+    const result = await httpClient.get(API_ENDPOINT);
+
+    if (result.statusCode == 200) return true;
+  } catch (err) {
+    return false;
+  }
+};
