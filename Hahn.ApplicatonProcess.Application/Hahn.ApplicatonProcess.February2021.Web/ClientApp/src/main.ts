4c408b8d0825c095cfcff803d9d8c4c3863804f1
okyere <clement.okyere@gmail.com>
2021-04-29 16:00:35 +0000

    removed dead comments

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/ClientApp/src/main.ts
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.February2021.Web/ClientApp/src/main.ts
@@ -1,7 +1,7 @@
 import {Aurelia} from 'aurelia-framework';
 import * as environment from '../config/environment.json';
 import { PLATFORM } from 'aurelia-pal';
-import { TCustomAttribute } from "aurelia-i18n";
+import { I18N, TCustomAttribute } from "aurelia-i18n";
 import XHR from "i18next-xhr-backend";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
@@ -17,21 +17,17 @@ export function configure(aurelia: Aurelia): void {
       TCustomAttribute.configureAliases(aliases);
 
       // register backend plugin
-      // instance.i18next.use(Backend.with(aurelia.loader));
-      // register backend plugin
       instance.i18next.use(XHR);
 
-      // adapt options to your needs (see http://i18next.com/docs/options/)
-      // make sure to return the promise of the setup method, in order to guarantee proper loading
       return instance.setup({
         backend: {
           // <-- configure backend settings
-          loadPath: "/locales/{{lng}}/{{ns}}.json", // <-- XHR settings for where to get the files from
+          loadPath: "./locales/{{lng}}/{{ns}}.json", // <-- XHR settings for where to get the files from
         },
         attributes: aliases,
         lng: "de",
         fallbackLng: "en",
-        debug: false,
+        debug: true,
       });
     })
     .feature(PLATFORM.moduleName("resources/index"));
